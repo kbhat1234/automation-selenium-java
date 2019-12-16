c39f8aad2a71946cc1939b047ef4206393bb8a75
kbhat1234 <karthikbhat@live.com>
2019-12-16 16:12:14 +0530

    add

---
+++ src/main/java/StaticBlock.java
@@ -0,0 +1,19 @@
+
+public class StaticBlock {
+	
+	static int a;
+	 static {
+		 a=10;
+		System.out.println("static block is invoked");
+		System.exit(0);
+	}
+	
+	//public static void main(String args[]) {
+		//StaticBlock sb = new StaticBlock();
+		//System.out.println("Main Program invoked");
+		//System.out.println(StaticBlock.a);
+		//System.out.println(sb.a);
+		
+	//}
+
+}
